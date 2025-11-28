5f009e39350a073fdad247cd027f909c511e87e9
joão Sátiro Bompastor <joao.s.bompastor@gmail.com>
2025-11-28 11:55:35 -0300

    initial right shift function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> (){
+    println!("{}", right_shift(1, 1));
+    println!("{}", right_shift(1, 2));
+    println!("{}", right_shift(1, 3));
+}
+
+
+fn right_shift(number: u8, shift: u8) -> u8 {
+    return number << shift as u8;
 }
